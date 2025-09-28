0740e355bd0522e7d7083fa3021a84e45a5c3b3f
eceliuns <221684513+eceliuns@users.noreply.github.com>
2025-09-28 18:45:35 +0200

    Set up MongoDB Atlas and add mongo.js script for phonebook CLI

---
+++ mongo.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://phonebook_project:${password}@cluster0.z5qyaoo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (name && number) {
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person
+    .save()
+    .then((result) => {
+      console.log(`added ${name} number ${number} to phonebook`);
+      mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.error("Error adding person:", err.message);
+      mongoose.connection.close();
+    });
+} else {
+  Person.find({})
+    .then((result) => {
+      console.log("phonebook:");
+      result.forEach((person) => {
+        console.log(`${person.name} ${person.number}`);
+      });
+      mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.error("Error fetching phonebook:", err.message);
+      mongoose.connection.close();
+    });
+}
